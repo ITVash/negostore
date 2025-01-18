5501897c64b0cc7de26894989b667927ea96cf45
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-18 14:30:55 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { useTelegram } from "@/shared/lib/providers"
 import { CreateUser } from "@/shared/lib/registerUser"
-import Image from "next/image"
 import React from "react"
 
 export default function Home() {
