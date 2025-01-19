f8f0b4c7982324635030ee6f075218bb53e30e2c
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-19 20:28:39 +0300

    Фронтенд 1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,23 +2,38 @@
 import { Container } from "@/shared/components/shared"
 import { useTelegram } from "@/shared/lib/providers"
 import { CreateUser } from "@/shared/lib/registerUser"
+import { useUser } from "@/shared/store"
 import React from "react"
 
 export default function Home() {
 	const { user, webApp } = useTelegram()
+	const { users, getMe, fetchUser, fetchUsersAll } = useUser()
+	React.useEffect(() => {
+		fetchUsersAll()
+	}, [])
+
 	React.useEffect(() => {
 		if (user) {
-			/* const userCreate = items.filter((item) => item.id_tg === user?.id)[0]
+			const userCreate = users.filter((item) => item.id_tg === user?.id)[0]
 			if (!userCreate) {
-      } */
-			//fetchUser(user.id)
-			CreateUser(user)
+				CreateUser(user)
+			}
+			fetchUser(user.id)
 		}
 		//fetchUser(454135208)
 	}, [user])
-	if (!webApp) {
+	React.useEffect(() => {
+		if (webApp && user) {
+			webApp!.BackButton.isVisible = false
+		}
+	}, [])
+	if (!webApp || !users || !getMe!.role) {
 		return <div>Загрузка...</div>
 	}
-	return <Container>привет</Container>
+	return (
+		<Container
+			className={`text-[#ffffff] text-[${webApp?.themeParams.text_color}] flex-col max-h-screen`}>
+			привет
+		</Container>
+	)
 }
-
