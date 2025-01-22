e85d95b885279d80454b6d376500c06db6f49f43
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-22 22:00:02 +0300

    First Display2

--- app/(root)/storehouse/[id]/page.tsx
+++ app/(root)/storehouse/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/shared/lib/utils"
 import React from "react"
 
-interface IStoreProps {
+/* interface IStoreProps {
 	className?: string
 	params?: { id: string }
 }
-
+ */
 /* export const StoreDetail: React.FC<IStoreProps> = ({ className, params }) => {
 	return <div className={cn("", className)}>Store {params?.id}</div>
 } */
