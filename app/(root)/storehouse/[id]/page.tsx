928242b20102fc356b0b0cfc978a7170fbf00244
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-22 21:50:47 +0300

    First Display

--- app/(root)/storehouse/[id]/page.tsx
+++ app/(root)/storehouse/[id]/page.tsx
@@ -3,9 +3,9 @@ import React from "react"
 
 interface IStoreProps {
 	className?: string
-	params: { id: string }
+	params?: { id: string }
 }
 
 export const StoreDetail: React.FC<IStoreProps> = ({ className, params }) => {
-	return <div className={cn("", className)}>Store {params.id}</div>
+	return <div className={cn("", className)}>Store {params?.id}</div>
 }
