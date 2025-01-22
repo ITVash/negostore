4f820161f78b8124625aec4b18955db1076626a3
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-22 22:07:35 +0300

    First Display2

--- app/(root)/storehouse/[id]/page.tsx
+++ app/(root)/storehouse/[id]/page.tsx
@@ -1,19 +1,36 @@
+import { Container, Loading } from "@/shared/components/shared"
+import { useTelegram } from "@/shared/lib/providers"
 import { cn } from "@/shared/lib/utils"
+import { useRouter } from "next/navigation"
 import React from "react"
-
-/* interface IStoreProps {
-	className?: string
-	params?: { id: string }
-}
- */
-/* export const StoreDetail: React.FC<IStoreProps> = ({ className, params }) => {
-	return <div className={cn("", className)}>Store {params?.id}</div>
-} */
 
 export default function StoreDetail({
 	params: { id },
 }: {
 	params: { id: string }
 }) {
-	return <div className={cn("")}>Store {id}</div>
+	const { webApp } = useTelegram()
+	const router = useRouter()
+
+	React.useEffect(() => {
+		if (webApp) {
+			webApp.BackButton.isVisible = true
+
+			webApp.onEvent("backButtonClicked", router.back)
+		}
+		return () => {
+			if (webApp) {
+				webApp.BackButton.isVisible = false
+				webApp.offEvent("backButtonClicked", router.back)
+			}
+		}
+	}, [])
+	if (!webApp) {
+		return <Loading />
+	}
+	return (
+		<Container className={cn(`text-[${webApp.themeParams.text_color}]`)}>
+			Store {id}
+		</Container>
+	)
 }
