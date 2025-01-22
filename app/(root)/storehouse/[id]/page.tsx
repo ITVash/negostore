8b34696c9d34c4e55c3358016b796800b54036de
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-22 21:53:15 +0300

    First Display

--- app/(root)/storehouse/[id]/page.tsx
+++ app/(root)/storehouse/[id]/page.tsx
@@ -6,6 +6,14 @@ interface IStoreProps {
 	params?: { id: string }
 }
 
-export const StoreDetail: React.FC<IStoreProps> = ({ className, params }) => {
+/* export const StoreDetail: React.FC<IStoreProps> = ({ className, params }) => {
 	return <div className={cn("", className)}>Store {params?.id}</div>
+} */
+
+export default function StoreDetail({
+	params: { id },
+}: {
+	params: { id: string }
+}) {
+	return <div className={cn("")}>Store {id}</div>
 }
