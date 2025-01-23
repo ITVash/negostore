4b9c4769c685022d3a9615aea0d7387f58303188
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-23 18:01:33 +0300

    First Display2

--- app/(root)/not-access/page.tsx
+++ app/(root)/not-access/page.tsx
@@ -1,8 +1,11 @@
-import { Container, NotAccess } from "@/shared/components/shared"
+import { Container, Loading, NotAccess } from "@/shared/components/shared"
 import { useTelegram } from "@/shared/lib/providers"
 
 export default function NotAccessPage() {
 	const { webApp } = useTelegram()
+	if (!webApp) {
+		return <Loading />
+	}
 	return (
 		<Container
 			className={`text-[#ffffff] text-[${webApp?.themeParams.text_color}] flex-col max-h-screen`}>
