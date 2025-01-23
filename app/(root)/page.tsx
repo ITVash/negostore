0292310d4dad66d1062684ec02b21bcbebfb9180
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-23 18:33:44 +0300

    First Display2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,12 +10,10 @@ import {
 import { useTelegram } from "@/shared/lib/providers"
 import { CreateUser } from "@/shared/lib/registerUser"
 import { useUser } from "@/shared/store"
-//import { useRouter } from "next/navigation"
 
 export default function Home() {
 	const { user, webApp } = useTelegram()
 	const { users, getMe, fetchUser, fetchUsersAll } = useUser()
-	//const router = useRouter()
 
 	React.useEffect(() => {
 		fetchUsersAll()
@@ -26,10 +24,7 @@ export default function Home() {
 			const userCreate = users.filter((item) => item.id_tg === user?.id)[0]
 			if (!userCreate) {
 				CreateUser(user)
-				//router.push("/not-access")
 			}
-			const redirect = users.filter((item) => item.id_tg === user.id)
-			//if (redirect.length <= 0) router.push("/not-access")
 			fetchUser(user.id)
 		}
 		//fetchUser(454135208)
