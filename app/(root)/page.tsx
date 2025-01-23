80d967869d6bd85ce4f420a04c85889fb60aed36
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-23 18:11:03 +0300

    First Display2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
 
 	React.useEffect(() => {
 		fetchUsersAll()
-		console.log(users)
 	}, [])
 
 	React.useEffect(() => {
@@ -27,7 +26,7 @@ export default function Home() {
 			const userCreate = users.filter((item) => item.id_tg === user?.id)[0]
 			if (!userCreate) {
 				CreateUser(user)
-				router.push("/not-access")
+				!userCreate && router.push("/not-access")
 			}
 			fetchUser(user.id)
 		}
