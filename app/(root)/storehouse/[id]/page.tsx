9ed088083cc7e79664d96952abfe3f1b09ac00a2
ITVash <49442123+ITVash@users.noreply.github.com>
2025-01-27 21:25:44 +0300

    First Display4

--- app/(root)/storehouse/[id]/page.tsx
+++ app/(root)/storehouse/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Container, Loading } from "@/shared/components/shared"
 import { Button } from "@/shared/components/ui/button"
+import { Input } from "@/shared/components/ui/input"
 import {
 	Tabs,
 	TabsContent,
@@ -10,6 +11,7 @@ import {
 import { useTelegram } from "@/shared/lib/providers"
 import { cn } from "@/shared/lib/utils"
 import { useStore } from "@/shared/store"
+import { Search, X } from "lucide-react"
 import { useRouter } from "next/navigation"
 import React from "react"
 
@@ -20,7 +22,16 @@ export default function StoreDetail({
 }) {
 	const { webApp } = useTelegram()
 	const { fetchStoreName, storeName } = useStore()
+	const [searchInput, setSearchInput] = React.useState<string>("")
 	const router = useRouter()
+
+	const searchHadler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchInput(e.target.value)
+	}
+
+	const delitSearchInput = () => {
+		setSearchInput("")
+	}
 
 	React.useEffect(() => {
 		fetchStoreName(Number(id))
@@ -58,10 +69,31 @@ export default function StoreDetail({
 				<TabsContent value='orders'>
 					Счета покупателей в разработке!!!
 				</TabsContent>
-				<TabsContent value='stores'>
-					<div className='flex justify-between'>
+				<TabsContent value='stores' className='flex justify-center'>
+					<div className='flex justify-between mt-3'>
 						<Button>Добавить товар</Button>
 						<Button>Приход товара</Button>
+					</div>
+					<div className={cn("flex w-full relative items-center mt-4 mb-2")}>
+						<Search
+							className={cn("text-gray-500 absolute left-[5px] z-10")}
+							size={16}
+						/>
+						<Input
+							type='text'
+							className='w-full pl-6 pr-6'
+							placeholder='Название организации...'
+							value={searchInput}
+							onChange={searchHadler}
+						/>
+						<X
+							className={cn(
+								"absolute right-[5px] cursor-pointer invisible",
+								searchInput.length > 0 && "visible",
+							)}
+							size={16}
+							onClick={delitSearchInput}
+						/>
 					</div>
 				</TabsContent>
 			</Tabs>
